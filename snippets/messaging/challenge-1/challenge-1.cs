1ada661200d2b540603924485dd3513cc080cf29
Jan Dusek <j@koiru.com>
2018-09-19 14:12:20 -0700

    Added code snippets for challenge 1

---
+++ snippets/messaging/challenge-1/challenge-1.cs
@@ -0,0 +1,12 @@
+const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+const string authToken = "your_auth_token";
+TwilioClient.Init(accountSid, authToken);
+
+var localAvailableNumbers = LocalResource.Read("US", areaCode: 510);
+
+// Purchase the first number on the list
+var firstNumber = localAvailableNumbers.First();
+var incomingPhoneNumber = IncomingPhoneNumberResource.Create(
+            phoneNumber: firstNumber.PhoneNumber);
+
+Console.WriteLine(incomingPhoneNumber.Sid);
---
+++ snippets/messaging/challenge-1/challenge-1.js
@@ -0,0 +1,17 @@
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+// Find and then purchase a phone number
+client
+  .availablePhoneNumbers('US')
+  .local.list({
+    areaCode: '510',
+  })
+  .then(data => {
+    const number = data[0];
+    return client.incomingPhoneNumbers.create({
+      phoneNumber: number.phoneNumber,
+    });
+  })
+  .then(purchasedNumber => console.log(purchasedNumber.sid));
