fd386023be071437845bed3f3d5ea86a8700bb4b
Jan Dusek <j@koiru.com>
2018-09-19 14:41:02 -0700

    Added Send SMS challenge snippets

---
+++ snippets/messaging/send-sms/send-sms.cs
@@ -0,0 +1,11 @@
+const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+const string authToken = "your_auth_token";
+
+TwilioClient.Init(accountSid, authToken);
+
+var message = MessageResource.Create(
+from: new Twilio.Types.PhoneNumber("+15017122661"),
+    body: "Body",
+    to: new Twilio.Types.PhoneNumber("+15017122661")
+        );
+Console.WriteLine(message.Sid);
