bbf7718071d640d0683a62d40ac21db148b3c596
Jan Dusek <j@koiru.com>
2018-09-24 15:06:39 +0800

    Unified CC incoming_call endpoint snippets

--- snippets/contact-center/create-incoming_call-endpoint/create-incoming_call-endpoint.php
+++ snippets/contact-center/create-incoming_call-endpoint/create-incoming_call-endpoint.php
@@ -7,7 +7,7 @@ use Twilio\Rest\Client;
 use Twilio\Twiml;
 
 $response = new Twiml();
-$gather = $response->gather([numDigits=>'1', action=>'/enqueue_call']);
+$gather = $response->gather([numDigits=>'1', action=>'/enqueue_call', timeout=>'3']);
 $gather->say('Please select from the following departments');
 $gather->say('For sales press one, for support press two, for marketing press three');
 
