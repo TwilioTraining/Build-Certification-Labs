340541b435d2db586c5d869c35a91afb59ac8091
Jim Madden <jmadden@users.noreply.github.com>
2018-10-01 17:01:58 -0700

    Update forward-sms.js

--- snippets/messaging/forward-sms/forward-sms.js
+++ snippets/messaging/forward-sms/forward-sms.js
@@ -9,7 +9,7 @@ app.post('/sms', (req, res) => {
 
   const MessagingResponse = require('twilio').twiml.MessagingResponse;
   const response = new MessagingResponse();
-  const message = response.message({to:process.env.TWILIO_ACME_CALLERID});
+  const message = response.message({to:'Your Phone number string here'});
   
   message.body(`From: ${req.body.From} Message: ${req.body.Body}`);
 
