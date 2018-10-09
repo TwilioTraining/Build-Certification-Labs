8492481de50dc466684e30fdcbd82fcbff268bdd
Thomas Curtis <thomas.e.curtis@gmail.com>
2018-10-09 11:05:56 +0100

    Update create-task.java

--- snippets/contact-center/create-task/create-task.java
+++ snippets/contact-center/create-task/create-task.java
@@ -4,8 +4,8 @@ import com.twilio.rest.taskrouter.v1.workspace.Task;
 
 public class Example {
     // Find your Account Sid and Token at twilio.com/console
-    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-    public static final String AUTH_TOKEN = "your_auth_token";
+    public static final String ACCOUNT_SID = System.getenv("ACCOUNT_SID");
+    public static final String AUTH_TOKEN = System.getenv("AUTH_TOKEN");
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
