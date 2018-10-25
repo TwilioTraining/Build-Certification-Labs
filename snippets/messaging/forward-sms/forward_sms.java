2501e7b82dd4f12e3dc0eb29c1951231a1c763c1
Stacy David <31826992+tigerfarm@users.noreply.github.com>
2018-10-25 12:59:57 -0700

    Create forward_sms.java

---
+++ snippets/messaging/forward-sms/forward_sms.java
@@ -0,0 +1,25 @@
+package javatwsamples;
+
+import com.twilio.twiml.MessagingResponse;
+import com.twilio.twiml.messaging.Body;
+import com.twilio.twiml.messaging.Message;
+
+public class forward_sms {
+
+    public static void main(String[] args) {
+        
+            Body body = new Body
+                    .Builder("The Robots are coming! Head for the hills!")
+                    .build();
+            Message sms = new Message
+                    .Builder()
+                    .body(body)
+                    .build();
+            MessagingResponse twiml = new MessagingResponse
+                    .Builder()
+                    .message(sms)
+                    .build();
+            
+        System.out.println("+ TwiML: " + twiml.toXml());
+    }
+}
