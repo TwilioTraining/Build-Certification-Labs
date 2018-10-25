5bd201c439c345574e13e33b59bebed90b1d1f3f
Stacy David <31826992+tigerfarm@users.noreply.github.com>
2018-10-25 13:13:10 -0700

    Create conference.java

---
+++ snippets/voice/conference/conference.java
@@ -0,0 +1,22 @@
+package javatwsamples.voice;
+
+import com.twilio.twiml.voice.Dial;
+import com.twilio.twiml.VoiceResponse;
+import com.twilio.twiml.TwiMLException;
+import com.twilio.twiml.voice.Conference;
+
+public class conference {
+
+    public static void main(String[] args) {
+        
+        Conference conference = new Conference.Builder("Room 1234").build();
+        Dial dial = new Dial.Builder().conference(conference).build();
+        VoiceResponse response = new VoiceResponse.Builder().dial(dial).build();
+
+        try {
+            System.out.println("+ TwiML: " + response.toXml());
+        } catch (TwiMLException e) {
+            e.printStackTrace();
+        }
+    }
+}
