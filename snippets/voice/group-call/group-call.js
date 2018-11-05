14caf7c7f219861af78d617a92bbe47a9d74943a
Thomas Curtis <thomas.e.curtis@gmail.com>
2018-11-05 15:23:30 +1100

    fixed code

--- snippets/voice/group-call/group-call.js
+++ snippets/voice/group-call/group-call.js
@@ -3,7 +3,7 @@ app.use('/group_call', function(req, res) {
   const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
   const authToken = 'your_auth_token';
   const client = require('twilio')(accountSid, authToken);
-  var numbers = {"+1234567910", "+1234567911", "+1234567912"};
+  var numbers = ["+1234567910", "+1234567911", "+1234567912"];
 
   numbers.forEach(function(number){
 
@@ -18,3 +18,4 @@ app.use('/group_call', function(req, res) {
   });
 
 }
+       )
