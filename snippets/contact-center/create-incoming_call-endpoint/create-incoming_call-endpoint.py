4a61f28781091e416d0031eced0d8a7dfee38187
Tyler Johnson <terrabakky@users.noreply.github.com>
2018-11-06 14:41:31 +1100

    Update create-incoming_call-endpoint.py
    Fix indentation issues

--- snippets/contact-center/create-incoming_call-endpoint/create-incoming_call-endpoint.py
+++ snippets/contact-center/create-incoming_call-endpoint/create-incoming_call-endpoint.py
@@ -2,6 +2,7 @@
 def incoming_call():
       resp = VoiceResponse()
       with resp.gather(num_digits="1", action="/enqueue_call", timeout="3") as g:
-      g.say("Please select from the following departments", language="en")
-      g.say("For sales press one, for support press two, for marketing press three", language="en")
-   return Response(str(resp), mimetype='text/xml')
+          g.say("Please select from the following departments", language="en")
+          g.say("For sales press one, for support press two, for marketing press three", language="en")
+        
+      return Response(str(resp), mimetype='text/xml')
