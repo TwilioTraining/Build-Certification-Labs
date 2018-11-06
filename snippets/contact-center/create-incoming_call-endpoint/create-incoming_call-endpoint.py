2b8f1e25d21455050c2d23ab21880370746c8ac4
Thomas Curtis <thomas.e.curtis@gmail.com>
2018-11-06 12:03:55 +1100

    updated formatting
    whitespace issues

--- snippets/contact-center/create-incoming_call-endpoint/create-incoming_call-endpoint.py
+++ snippets/contact-center/create-incoming_call-endpoint/create-incoming_call-endpoint.py
@@ -1,7 +1,7 @@
 @app.route("/incoming_call", methods=['GET', 'POST'])
 def incoming_call():
       resp = VoiceResponse()
-   with resp.gather(num_digits="1", action="/enqueue_call", timeout="3") as g:
+      with resp.gather(num_digits="1", action="/enqueue_call", timeout="3") as g:
       g.say("Please select from the following departments", language="en")
       g.say("For sales press one, for support press two, for marketing press three", language="en")
    return Response(str(resp), mimetype='text/xml')
