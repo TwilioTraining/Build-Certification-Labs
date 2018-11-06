d1f4fb4fa82fbf3ba10a49be50c90e1497c480c6
Thomas Curtis <thomas.e.curtis@gmail.com>
2018-11-06 14:58:41 +1100

    updating
    clarity

--- snippets/contact-center/create-enqueue_call-endpoint/create-enqueue_call-endpoint.js
+++ snippets/contact-center/create-enqueue_call-endpoint/create-enqueue_call-endpoint.js
@@ -1,15 +1,15 @@
 app.use('/enqueue_call', function(req, res){
 
 const response = new VoiceResponse();
-var Digits = req.query.Digits;
+var Digits = req.body.Digits;
 
 var product = {
       1: 'sales',
       2: 'support',
       3: 'marketing'
-    }
+    };
     
-    const enqueue = response.enqueue({workflowSid:workflow_sid });  
+    const enqueue = response.enqueue({workflowSid:"WWXXXXXXXXXXXXXXXXXXXXX" });  
     enqueue.task({}, JSON.stringify({selected_product: product[Digits]}));
 
     res.type('text/xml');
