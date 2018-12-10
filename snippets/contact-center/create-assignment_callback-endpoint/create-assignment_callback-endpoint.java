d62d8523894d3a15e1f35f5c65d16ddbd8fb29d0
Twilio CMS User <jmadden@twilio.com>
2018-12-10 13:30:54 -0800

    CMS update of snippets/contact-center/create-assignment_callback-endpoint by jmadden@twilio.com

--- snippets/contact-center/create-assignment_callback-endpoint/create-assignment_callback-endpoint.java
+++
@@ -1,8 +0,0 @@
-public String assignmentCallback() {
-    Map<String, String> dequeueInstruction = new HashMap<String, String>();
-    dequeueInstruction.put("instruction", "dequeue");
-    dequeueInstruction.put("from", "+112345678910");
-    dequeueInstruction.put("post_work_activity_sid", "WAXXXXXXXXXXXXXXXXXXXXX");
-    response.setContentType("application/json");
-    response.getWriter().print(JSONObject.toJSONString(dequeueInstruction));
-}
