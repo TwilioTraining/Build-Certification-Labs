378e14dd8af2f1f2298360d1da9310634a1d3833
nahusznaj <nsznaj@gmail.com>
2019-01-29 15:04:42 -0800

    suggest edit to the example
    This is the conference case for https://www.twilio.com/docs/build/voice-labs

--- snippets/voice/conference/conference.py
+++ snippets/voice/conference/conference.py
@@ -1,7 +1,11 @@
+#add this import from twilio.twiml.voice_response import Conference, Dial, VoiceResponse
+
+
 @app.route("/conference", methods=['GET', 'POST'])
 def conference():
    resp = VoiceResponse()
    dial = Dial()
    dial.conference('trainingconference')
    resp.append(dial)
-   return Response(str(resp), mimetype='text/xml')
+   return str(resp)
+
