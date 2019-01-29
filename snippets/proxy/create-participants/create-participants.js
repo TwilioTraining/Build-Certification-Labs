0d25138bcb2b85be955db7f845ecd97c441e1ac5
Jim Madden <jmadden@users.noreply.github.com>
2019-01-29 11:16:46 -0800

    Update create-participants.js

--- snippets/proxy/create-participants/create-participants.js
+++ snippets/proxy/create-participants/create-participants.js
@@ -1,3 +1,6 @@
+// If you do not already have it installed, you will need twilio-node 
+// installed to run this code: https://www.npmjs.com/package/twilio
+
 const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 const authToken = 'your_auth_token';
 const client = require('twilio')(accountSid, authToken);
