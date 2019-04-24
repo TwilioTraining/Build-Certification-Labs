744ea360715aa86480b5dbf3dde7ac82c1f5c266
Jim Madden <jmadden@users.noreply.github.com>
2019-04-23 22:59:29 -0700

    Update create-incoming_call-endpoint.js

--- snippets/contact-center/create-incoming_call-endpoint/create-incoming_call-endpoint.js
+++ snippets/contact-center/create-incoming_call-endpoint/create-incoming_call-endpoint.js
@@ -1,11 +1,11 @@
-app.use('/incoming_call', function(req, res) {
+app.use('/incoming-call', function(req, res) {
   const VoiceResponse = require('twilio').twiml.VoiceResponse;
   const response = new VoiceResponse();
   const gather = response.gather({
     input: 'speech dtmf',
     timeout: 3,
     numDigits: 1,
-    action:'/enqueue_call'
+    action:'/enqueue-call'
   });
   gather.say('Please select from the following departments');
   gather.say('For sales press one, for support press two, for marketing press three');
