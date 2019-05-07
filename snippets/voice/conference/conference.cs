5c43c3c6334f11081f6611cb8918533fe14fa016
Twilio CMS User <jmadden@twilio.com>
2019-05-07 15:58:23 -0700

    CMS update of snippets/voice/conference by jmadden@twilio.com

--- snippets/voice/conference/conference.cs
+++
@@ -1,9 +0,0 @@
-public ActionResult conference()
-{
-    var response = new VoiceResponse();
-    var dial = new Dial();
-    dial.Conference(name: "trainingConference");
-    response.Append(dial);
-    return Content(response.ToString(), contentType: "text/xml");
-}
-
