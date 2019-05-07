96140954b42f0daacc2ed2b0f366090d2ed18b01
Thomas Curtis <thomas.e.curtis@gmail.com>
2019-05-07 09:28:19 +0100

    removed post work activity sid

--- snippets/contact-center/create-assignment_callback-endpoint/create-assignment_callback-endpoint.js
+++ snippets/contact-center/create-assignment_callback-endpoint/create-assignment_callback-endpoint.js
@@ -1,6 +1,6 @@
 app.use('/assignment_callback', function(req, res){
 // add your Twilio phone number and WrapUp activity sid 
-var dequeue = {"instruction": "dequeue", "from": "+12345678910", "post_work_activity_sid": "WAXXXXXXXXXXXXXXXXXXXXX"}
+var dequeue = {"instruction": "dequeue", "from": "+12345678910"}
 
 res.type('application/json');
 
