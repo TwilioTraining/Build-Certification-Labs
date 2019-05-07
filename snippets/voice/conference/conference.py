34a8473a0581460b8822b5333bb915bddaedd98a
Twilio CMS User <jmadden@twilio.com>
2019-05-07 15:58:26 -0700

    CMS update of snippets/voice/conference by jmadden@twilio.com

--- snippets/voice/conference/conference.py
+++
@@ -1,7 +0,0 @@
-@app.route("/conference", methods=['GET', 'POST'])
-def conference():
-   resp = VoiceResponse()
-   dial = Dial()
-   dial.conference('trainingconference')
-   resp.append(dial)
-   return Response(str(resp), mimetype='text/xml')
