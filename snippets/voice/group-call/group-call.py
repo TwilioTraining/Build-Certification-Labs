e88d7ac15dd02b51be2c27f956454f11a2d82542
Jim Madden <jmadden@users.noreply.github.com>
2019-05-08 12:40:40 -0700

    Update group-call.py

--- snippets/voice/group-call/group-call.py
+++ snippets/voice/group-call/group-call.py
@@ -1,14 +1,19 @@
-@app.route("/group_call", methods=['GET', 'POST'])
-def dial_group():
-  
-   #http://916d20ff.ngrok.io/
-   # Create a group call endpoint that calls anyone that has messaged my Twilio number today
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
 
-   resp = VoiceResponse()
-   numbers = ['+12345678910', '+12345678911', '+12345678912']
-   for number in numbers:
-       client.calls.create(
-           from_ = caller_id,
-           to=number,
-           url='http://916d20ff.ngrok.io/conference'
-       )
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+client = Client(account_sid, auth_token)
+
+numbers = ['+12345678910', '+12345678911', '+12345678912']
+
+for number in numbers:
+  call = client.calls.create(
+    url = 'http://demo.twilio.com/docs/voice.xml', # Your TwiML Bin URL here.
+    to = number,
+    from_ = '+15017122661' # Your Twilio phone number here.
+  )
+  print(call.sid)
