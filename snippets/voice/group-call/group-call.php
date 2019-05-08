22783c8068eaffa5e934c4586a026bc5133b8a02
Jim Madden <jmadden@users.noreply.github.com>
2019-05-08 12:36:58 -0700

    Update group-call.php

--- snippets/voice/group-call/group-call.php
+++ snippets/voice/group-call/group-call.php
@@ -1,18 +1,26 @@
 <?php
-require_once '/path/to/vendor/autoload.php'; // Loads the library
+
+// Update the path below to your autoload.php,
+// see https://getcomposer.org/doc/01-basic-usage.md
+require_once '/path/to/vendor/autoload.php';
+
 use Twilio\Rest\Client;
-// Your Account Sid and Auth Token from twilio.com/user/account
-$response = new TwiML();
-$sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-$token = "your_auth_token";
-$client = new Client($sid, $token);
+
+// Find your Account Sid and Auth Token at twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+$sid    = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$token  = "your_auth_token";
+$twilio = new Client($sid, $token);
+
 $numbers = array("+1234567910", "+1234567911", "+1234567912");
+
 foreach ($numbers as $number)
 {
-    $call = $client->calls->create(
-        $number, 
-        "+14158675310",
-        array("url" => "http://yourdomain.com/conference")
+    $call = $twilio->calls
+        ->create($number,
+            "+15017122661", // Your Twilio phone number.
+            array("url" => "http://demo.twilio.com/docs/voice.xml") // Your TwiML Bin URL here.
     );
+
+    print($call->sid);
 }
-echo $response;
