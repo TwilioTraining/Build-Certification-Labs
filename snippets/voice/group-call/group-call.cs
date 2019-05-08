5843c6c688b0045ad433b121057c41920343db02
Jim Madden <jmadden@users.noreply.github.com>
2019-05-08 12:34:24 -0700

    Update group-call.cs

--- snippets/voice/group-call/group-call.cs
+++ snippets/voice/group-call/group-call.cs
@@ -1,19 +1,31 @@
-public ActionResult groupCall()
-        {
-            var response = new VoiceResponse();
+// Install the C# / .NET helper library from twilio.com/docs/csharp/install
 
-            string[] numbers = {"+1234567910", "+1234567911", "+1234567912"};
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
-            TwilioClient.Init(_accountSid, _authToken);
-           
-            foreach (string number in numbers) {
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        // Find your Account Sid and Token at twilio.com/console
+        // DANGER! This is insecure. See http://twil.io/secure
+        const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string authToken = "your_auth_token";
+            
+        string[] numbers = {"+1234567910", "+1234567911", "+1234567912"};
+
+        TwilioClient.Init(accountSid, authToken);
+        
+        foreach (string number in numbers) {
                 var call = CallResource.Create(
-                    url: new Uri("http://example.com/conference"),
+                    url: new Uri("http://demo.twilio.com/docs/voice.xml"), // Your TwiML Bin URL here.
                     to: new Twilio.Types.PhoneNumber(number),
-                    from: new Twilio.Types.PhoneNumber(_called_id)
+                    from: new Twilio.Types.PhoneNumber("+15017122661") // Your Twilio phone number here.
                 );
-            }
 
-            return Content(response.ToString(), contentType: "text/xml");
+                Console.WriteLine(call.Sid);
+                
         }
-
+}
